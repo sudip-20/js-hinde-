8636364823d206946d4dda3a69497067ead16f74
sudip-20 <sudiptobcy0007@gmail.com>
2025-08-17 12:45:10 +0000

    datatype

---
+++ o1_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 147895
+let accountMail = "sudip@google.com"
+var accountPass = "1234"
+city = "banglore"
+let accountState;
+
+console.table([accountId , accountMail , accountPass , city , accountState])
