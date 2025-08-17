39348ef59f5c2e65c4185a26a94e2ca8a1db351d
sudip-20 <sudiptobcy0007@gmail.com>
2025-08-17 13:04:29 +0000

    converstion of data types

---
+++ o1_basics/03_converstion.js
@@ -0,0 +1,15 @@
+let score = "22abs"
+let ValueInNumber = Number(score)
+console.log(typeof  ValueInNumber);
+console.log(ValueInNumber);
+
+
+let LoggedIn = 1 
+let cooleanIsLoggedIn = Boolean(LoggedIn)
+console.log(cooleanIsLoggedIn);
+
+
+// 1 => true 
+// 0 => false 
+// "" => false 
+// "hitesh" => true
