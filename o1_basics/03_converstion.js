00f09dcd0dc6514cc3dfd3f1f0268ec086597e4a
sudip-20 <sudiptobcy0007@gmail.com>
2025-08-17 13:17:42 +0000

    convertion data type

--- o1_basics/03_converstion.js
+++ o1_basics/03_converstion.js
@@ -1,15 +1,33 @@
 let score = "22abs"
 let ValueInNumber = Number(score)
-console.log(typeof  ValueInNumber);
-console.log(ValueInNumber);
+// console.log(typeof  ValueInNumber);
+// console.log(ValueInNumber);
 
 
 let LoggedIn = 1 
 let cooleanIsLoggedIn = Boolean(LoggedIn)
-console.log(cooleanIsLoggedIn);
+// console.log(cooleanIsLoggedIn);
 
 
 // 1 => true 
 // 0 => false 
 // "" => false 
 // "hitesh" => true
+
+ //*******************operation ************
+
+  let value = 3
+  let negvalue = -value 
+  console.log(negvalue);
+  
+
+  let str1 = "hello"
+  let str2 = " sudip"
+  let str3  = str1  + str2
+  console.log(str3);
+  
+
+  let gameCounter = 100
+  gameCounter++;
+  console.log(gameCounter);
+  
