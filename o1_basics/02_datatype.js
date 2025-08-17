c0ada21bba7ad970e6d5a2a3770b365f6ab7b403
sudip-20 <sudiptobcy0007@gmail.com>
2025-08-17 12:43:57 +0000

    datatype

---
+++ o1_basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+ let name = "sudip" 
+ let age = 18  
+ let isLoggedIn = false
+ let state  = null
+ let city;
+
+
+// number => 2 to power 53
+//bigint 
+//string = ""
+// boolean = true/false
+//null = standalone value
+// undefined =>
+    // symbol => unique 
+//object 
+
+
+console.log(typeof "sudip");
