67b2e75a728980815784d9a0ff0d3b27c2d7b6f4
sudip-20 <sudiptobcy0007@gmail.com>
2025-08-19 10:11:02 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,33 @@
+const myArr = [ 0 , 2 , 8, 7 , 6]
+// console.log(myArr[1]);
+
+
+// aray methods
+
+myArr.push(65)
+myArr.pop()
+myArr.unshift(67)
+myArr.shift()
+// console.log(myArr);
+
+
+// console.log(myArr.includes(65));
+// console.log(myArr.indexOf(6));
+
+const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+
+//slice . spice 
+
+console.log("A" , myArr);
+const mya1 = myArr.slice(1,3)
+console.log(mya1);
+console.log("b" , myArr);
+
+
+
+
+
+
