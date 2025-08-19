4e2dd14d5a57787efa5dfee1de340655255d1981
sudip-20 <sudiptobcy0007@gmail.com>
2025-08-19 09:30:57 +0000

    math_num

---
+++ o1_basics/06_nums_math.js
@@ -0,0 +1,29 @@
+const score = 200
+
+const balance = new Number(100)
+
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8677
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//==============================//==============================
+
+
+console.log(Math)
+console.log(Math.abs(-5));
+console.log(Math.round(4.6));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
