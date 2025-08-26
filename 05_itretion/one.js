274544579a70f22ce4c7f5ae739cb90bce0bf97a
sudip-20 <sudiptobcy0007@gmail.com>
2025-08-26 16:13:05 +0000

    loop

---
+++ 05_itretion/one.js
@@ -0,0 +1,37 @@
+//for
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is the best number ");  
+//     }
+//     console.log(element);    
+// }
+
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`outher loop ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//         console.log(i + '*' + j + "=" + i*j);    
+//     }
+// }
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 5){
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`VAlue of i is ${i}`);  
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 5){
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//     console.log(`VAlue of i is ${i}`);  
+// }
+
