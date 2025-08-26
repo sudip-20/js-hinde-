1494bbf7f42e65a81f7c57f69e059340536cb323
sudip-20 <sudiptobcy0007@gmail.com>
2025-08-26 15:44:22 +0000

    control flow

---
+++ 04_controlflow/one.js
@@ -0,0 +1,59 @@
+//if
+
+// if(Condition){
+
+// }
+
+// const isuserloggin = true 
+// const temp = 48
+
+// if (temp > 50){
+//     console.log(`tem is higher than 50`);
+// }
+// else{
+//     console.log(`remp is lower than 50`);
+    
+// }
+
+
+// const score = 200
+// if (score >100){
+//     let power = "fly"
+//     console.log(`user power ${power}`);
+//}
+
+// const balance = 1000
+// if(balance>500) console.log("test"),
+// console.log("test2");
+
+
+
+
+// const balance = 10000
+
+// if(balance<500){
+//     console.log(`less than 500`);
+// }
+// else if(balance < 1000){
+//     console.log(`less than 1000`);
+// }
+// else{
+//     console.log(`balace is lace than 15000`);
+// }
+
+
+
+// const userLoggin = true
+// const debitCard = true
+// const loggedInfromGoogle = false
+// const mail = true
+
+// if(userLoggin && debitCard){
+//     console.log(`User is loggedin`);
+// }
+// if(loggedInfromGoogle || mail){
+//     console.log(`user is logged in`);
+    
+// }
+
+
