06c567a85348bbad2a8a3091184ee69ff94561cd
sudip-20 <sudiptobcy0007@gmail.com>
2025-08-26 10:13:33 +0000

    scops

---
+++ o3_basics/scops.js
@@ -0,0 +1,36 @@
+// let a = 10
+// const b = 20
+// var c = 30
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//  function one(){
+//     const userName = "sudip"
+    
+//     function two(){
+//         const web = "google"
+//         console.log(userName);
+        
+//     }
+//     two()
+    
+//  }
+//  one()
+
+
+// if(true) {
+//     const userName = "sudip"
+//     if(userName === "sudip"){
+//         const web = " google"
+//         console.log(userName + web );
+        
+//     }
+// }
+
+addtwo(6)
+const addtwo = function(num){
+    return num+2
+} 
+
